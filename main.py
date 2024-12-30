7d3c043c22a8e89028a9ab73cccb4fde0f25e82e
Black-Seraph <admin@black-seraph.com>
2024-12-30 18:19:24 +0100

    Initial build

--- main.py
+++ main.py
@@ -1,57 +1,90 @@
 import os
-
-# The decky plugin module is located at decky-loader/plugin
-# For easy intellisense checkout the decky-loader code repo
-# and add the `decky-loader/plugin/imports` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky
 import asyncio
+import subprocess
+import signal
+import psutil
 
 class Plugin:
-    # A normal method. It can be called from the TypeScript side using @decky/api.
-    async def add(self, left: int, right: int) -> int:
-        return left + right
+    async def startDBus(self):
+        try:
+            result = subprocess.run(
+                ["dbus-launch", "--sh-syntax"],
+                capture_output=True,
+                text=True,
+                check=True
+            )
+            for line in result.stdout.splitlines():
+                if line.startswith("DBUS_SESSION_BUS_ADDRESS") or line.startswith("DBUS_SESSION_BUS_PID"):
+                    key, value = line.split("=", 1)
+                    value = value.rstrip(";")
+                    if (value.startswith("'") and value.endswith("'")) or (value.startswith('"') and value.endswith('"')):
+                        value = value[1:-1]
+                    os.environ[key] = value
+        except Exception as e:
+            decky.logger.error(f"DBus couldn't be started: {str(e)}")
 
-    async def long_running(self):
-        await asyncio.sleep(15)
-        # Passing through a bunch of random data, just as an example
-        await decky.emit("timer_event", "Hello from the backend!", True, 2)
+    async def stopDBus(self):
+        dbus_pid = os.environ.get("DBUS_SESSION_BUS_PID")
+        if dbus_pid:
+            try:
+                dbus_pid = int(dbus_pid)
+                proc = psutil.Process(dbus_pid)
+                proc.terminate()
+                proc.wait()
+            except Exception as e:
+                pass
 
-    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
-    async def _main(self):
-        self.loop = asyncio.get_event_loop()
-        decky.logger.info("Hello World!")
+    async def daemonStop(self):
+        for proc in psutil.process_iter(['pid', 'name']):
+            try:
+                if proc.info['name'] == 'ghlble':
+                    proc.terminate()
+            except Exception as e:
+                continue
+        await self.stopDBus()
 
-    # Function called first during the unload process, utilize this to handle your plugin being stopped, but not
-    # completely removed
-    async def _unload(self):
-        decky.logger.info("Goodnight World!")
-        pass
+    async def daemonStart(self):
+        try:
+            await self.daemonStop()
+            await self.startDBus()
+            self.daemon = await asyncio.create_subprocess_exec(os.path.join(os.path.dirname(__file__), "bin/ghlble"), "--daemon", env=os.environ.copy())
+        except Exception as e:
+            decky.logger.error(f"Daemon couldn't be started: {str(e)}")
 
-    # Function called after `_unload` during uninstall, utilize this to clean up processes and other remnants of your
-    # plugin that may remain on the system
-    async def _uninstall(self):
-        decky.logger.info("Goodbye World!")
-        pass
+    async def daemonSetScan(self, enabled: bool):
+        if enabled:
+            await self.daemonStart()
+        else:
+            try:
+                await asyncio.create_subprocess_exec(os.path.join(os.path.dirname(__file__), "bin/ghlble"), "--scan=off", env=os.environ.copy())
+            except Exception as e:
+                decky.logger.warning(f"Scan couldn't be stopped: {str(e)}")
 
-    async def start_timer(self):
-        self.loop.create_task(self.long_running())
+    async def daemonGetScan(self):
+        try:
+            process = await asyncio.create_subprocess_exec(os.path.join(os.path.dirname(__file__), "bin/ghlble"), "--scan", env=os.environ.copy(), stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+            stdout, stderr = await process.communicate()
+            if process.returncode != 0:
+                return False
+            output = stdout.decode().strip()
+            if output == "Scan status: Scanning":
+                return True
+            elif output == "Scan status: Not scanning":
+                return False
+            else:
+                return False
+        except Exception as e:
+            return False
 
-    # Migrations that should be performed before entering `_main()`.
     async def _migration(self):
-        decky.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky.decky_LOG_DIR/template.log`
-        decky.migrate_logs(os.path.join(decky.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky.decky_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky.decky_SETTINGS_DIR/`
-        decky.migrate_settings(
-            os.path.join(decky.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-        decky.migrate_runtime(
-            os.path.join(decky.DECKY_HOME, "template"),
-            os.path.join(decky.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        pass
+
+    async def _main(self):
+        pass
+
+    async def _unload(self):
+        await self.daemonStop()
+
+    async def _uninstall(self):
+        pass
--- src/index.tsx
+++ src/index.tsx
@@ -1,115 +1,73 @@
 import {
-  ButtonItem,
   PanelSection,
   PanelSectionRow,
-  Navigation,
+  ToggleField,
   staticClasses
 } from "@decky/ui";
+
 import {
-  addEventListener,
-  removeEventListener,
   callable,
-  definePlugin,
-  toaster,
-  // routerHook
+  definePlugin
 } from "@decky/api"
-import { useState } from "react";
-import { FaShip } from "react-icons/fa";
 
-// import logo from "../assets/logo.png";
+import { useState, useEffect } from "react";
+import { FaGuitar } from "react-icons/fa";
 
-// This function calls the python function "add", which takes in two numbers and returns their sum (as a number)
-// Note the type annotations:
-//  the first one: [first: number, second: number] is for the arguments
-//  the second one: number is for the return value
-const add = callable<[first: number, second: number], number>("add");
-
-// This function calls the python function "start_timer", which takes in no arguments and returns nothing.
-// It starts a (python) timer which eventually emits the event 'timer_event'
-const startTimer = callable<[], void>("start_timer");
+const daemonGetScan = callable<[], boolean>("daemonGetScan");
+const daemonSetScan = callable<[enabled: boolean], void>("daemonSetScan");
 
 function Content() {
-  const [result, setResult] = useState<number | undefined>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [scan, setScan] = useState<boolean>(false);
 
-  const onClick = async () => {
-    const result = await add(Math.random(), Math.random());
-    setResult(result);
+  useEffect(() => {
+    const fetchInitialState = async () => {
+      try {
+        const initialState = await daemonGetScan();
+        setScan(initialState);
+      } catch (error) {
+        console.error("Failed to fetch initial scan state:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchInitialState();
+  }, []);
+
+  const onScanToggle = async (checked: boolean) => {
+    setScan(checked);
+    daemonSetScan(checked);
   };
 
+  if (loading) {
+    return <PanelSection title="Service Control">Loading...</PanelSection>;
+  }
+
   return (
-    <PanelSection title="Panel Section">
+    <PanelSection title="Service Control">
       <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={onClick}
-        >
-          {result ?? "Add two numbers via Python"}
-        </ButtonItem>
+        <ToggleField
+          label="Pair Guitars"
+          description="Enable, turn on your guitars, then disable once they are connected."
+          checked={scan}
+          onChange={onScanToggle}
+        />
       </PanelSectionRow>
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => startTimer()}
-        >
-          {"Start Python timer"}
-        </ButtonItem>
-      </PanelSectionRow>
-
-      {/* <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow> */}
-
-      {/*<PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Navigation.Navigate("/decky-plugin-test");
-            Navigation.CloseSideMenus();
-          }}
-        >
-          Router
-        </ButtonItem>
-      </PanelSectionRow>*/}
     </PanelSection>
   );
 };
 
 export default definePlugin(() => {
-  console.log("Template plugin initializing, this is called once on frontend startup")
-
-  // serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-  //   exact: true,
-  // });
-
-  // Add an event listener to the "timer_event" event from the backend
-  const listener = addEventListener<[
-    test1: string,
-    test2: boolean,
-    test3: number
-  ]>("timer_event", (test1, test2, test3) => {
-    console.log("Template got timer_event with:", test1, test2, test3)
-    toaster.toast({
-      title: "template got timer_event",
-      body: `${test1}, ${test2}, ${test3}`
-    });
-  });
+  console.log("Guitar Hero Live Plugin is loading...")
 
   return {
-    // The name shown in various decky menus
-    name: "Test Plugin",
-    // The element displayed at the top of your plugin's menu
-    titleView: <div className={staticClasses.Title}>Decky Example Plugin</div>,
-    // The content of your plugin's menu
+    name: "Guitar Hero Live",
+    titleView: <div className={staticClasses.Title}>Guitar Hero Live</div>,
     content: <Content />,
-    // The icon displayed in the plugin list
-    icon: <FaShip />,
-    // The function triggered when your plugin unloads
+    icon: <FaGuitar />,
     onDismount() {
-      console.log("Unloading")
-      removeEventListener("timer_event", listener);
-      // serverApi.routerHook.removeRoute("/decky-plugin-test");
+      console.log("Guitar Hero Live Plugin is unloading...")
     },
   };
 });
